0714d38de73fefec9323e0bfb4b0611fd98624e0
Florian Chevallier <fchevallier@atecna.fr>
2026-02-04 18:43:59 +0100

    fix: update Homebrew cask to use correct DMG filename (Meety-*.dmg)

--- Casks/meety.rb
+++ Casks/meety.rb
@@ -2,7 +2,7 @@ cask "meety" do
   version "0.1.18"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-  url "https://github.com/florianchevallier/meeting-recorder/releases/download/v#{version}/MeetingRecorder-#{version}.dmg"
+  url "https://github.com/florianchevallier/meeting-recorder/releases/download/v#{version}/Meety-#{version}.dmg"
   name "Meety"
   desc "Native macOS meeting recorder with Teams detection and system audio capture"
   homepage "https://github.com/florianchevallier/meeting-recorder"
