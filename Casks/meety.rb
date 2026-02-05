3776447d6203e2534f1a7d243eb5da9c226f9ceb
Florian Chevallier <fchevallier@atecna.fr>
2026-02-05 11:05:04 +0100

    chore: update Meety to v0.1.19

--- Casks/meety.rb
+++ Casks/meety.rb
@@ -1,6 +1,6 @@
 cask "meety" do
-  version "0.1.18"
-  sha256 "d372a6aecf07849d46a85e3592ae0026d99bfff91a315b88f5fe04e157a73eb7"
+  version "0.1.19"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/florianchevallier/meeting-recorder/releases/download/v#{version}/Meety-#{version}.dmg"
   name "Meety"
